a0d39a4bb0ff86538a1597146982c6ecd93db4c2
acolletti21 <ashleymcolletti@gmail.com>
2017-02-12 01:27:47 -0600

    added features

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
    authenticated :user do
@@ -10,4 +11,18 @@ Rails.application.routes.draw do
      root 'devise/registrations#new', as: :unauthenticated_root
    end
  end
+
+ get '/links' => 'links#index'
+ get '/links/new' => 'links#new'
+ 
+ post '/links' => 'links#create'
+ get '/links/:id' => 'links#show'
+
+ get 'links/:id/edit' => 'links#edit'
+ patch '/links/:id' => 'links#update'
+
+ delete '/links/:id' => 'links#destroy'
+ get '/users/sign_out' => 'users#destroy'
+ 
+
 end
